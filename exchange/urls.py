ff71c3d8a8b0185239f5c3508f6979e8811b594e
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2018-11-19 11:37:51 +0200

    commnet routes to custom views (new_map, new_map_json, layer_details)
    @davisc
    these views is working with geonode2.6 and not compatible with geonode2.8
    if custom logic needed we should get new view from geonode2.8
    new views should reviewed first as it may support our needs without customization

--- exchange/urls.py
+++ exchange/urls.py
@@ -26,7 +26,6 @@ from elasticsearch_app.urls import urlpatterns as search_urls
 from geonode.urls import urlpatterns as geonode_urls
 
 from exchange.maploom.urls import urlpatterns as maploom_urls
-# from exchange.services.views import edit_service
 from fileservice.urls import urlpatterns as fileservice_urls
 from thumbnails.urls import urlpatterns as thumbnail_urls
 from . import views
@@ -52,16 +51,14 @@ urlpatterns = patterns(
 
     url(r'^services/(?P<pk>\d+)/publish$',
         views.publish_service, name='publish_service'),
-    # url(r'^services/(?P<service_id>\d+)/edit$',
-    #     edit_service, name='edit_service'),
 
     url(r'^auth-failed/', views.AuthErrorPage.as_view(), name='auth_failed'),
     url(r'^about/', views.about_page, name='about'),
     url(r'^capabilities/', views.capabilities, name='capabilities'),
     url(r'^logout/', views.logout, name='exchange_logout'),
 
-    url(r'^maps/new$', views.new_map, name="new_map"),
-    url(r'^maps/new/data$', views.new_map_json, name='new_map_json'),
+    # url(r'^maps/new$', views.new_map, name="new_map"),
+    # url(r'^maps/new/data$', views.new_map_json, name='new_map_json'),
 
     url(r'^proxy/', views.proxy),
 
@@ -103,10 +100,10 @@ if 'osgeo_importer' in settings.INSTALLED_APPS:
     urlpatterns += osgeo_importer_urls
 
 # Layer detail override needs to come after the layer upload override
-urlpatterns += [
-    url(r'^layers/(?P<layername>[^/]*)$',
-        views.layer_detail, name="layer_detail"),
-]
+# urlpatterns += [
+#     url(r'^layers/(?P<layername>[^/]*)$',
+#         views.layer_detail, name="layer_detail"),
+# ]
 
 if 'nearsight' in settings.INSTALLED_APPS:
     from nearsight.urls import urlpatterns as nearsight_urls
