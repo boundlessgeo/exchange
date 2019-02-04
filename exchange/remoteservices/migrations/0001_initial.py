dffbae231933f28108ef605d2bfec6c19d34cb9a
hisham waleed karam <hisham.karam@cartologic.com>
2019-02-04 13:22:06 +0200

    add missing remote services migration files

---
+++ exchange/remoteservices/migrations/0001_initial.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('services', '0040_auto_20190204_1045'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ExchangeService',
+            fields=[
+                ('service_ptr', models.OneToOneField(parent_link=True,
+                                                     auto_created=True,
+                                                     primary_key=True,
+                                                     serialize=False,
+                                                     to='services.Service')),
+                ('classification', models.CharField(
+                    max_length=255, null=True, blank=True)),
+                ('provenance', models.CharField(
+                    max_length=255, null=True, blank=True)),
+                ('caveat', models.CharField(max_length=255, null=True,
+                                            blank=True)),
+                ('poc_name', models.CharField(max_length=255, null=True,
+                                              blank=True)),
+                ('poc_position', models.CharField(
+                    max_length=255, null=True, blank=True)),
+                ('poc_email', models.CharField(
+                    max_length=255, null=True, blank=True)),
+                ('poc_phone', models.CharField(
+                    max_length=255, null=True, blank=True)),
+                ('poc_address', models.CharField(
+                    max_length=255, null=True, blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('services.service',),
+        ),
+    ]
