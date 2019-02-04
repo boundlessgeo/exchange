c2f061471f61f0dd19b5f74a26933a9ff45cc374
hisham waleed karam <hisham.karam@cartologic.com>
2019-02-04 14:14:33 +0200

    fix missing migration files

---
+++ exchange/core/migrations/0013_auto_20190204_1203.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import exchange.core.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0012_adds_content_creator_group'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='thumbnailimage',
+            name='thumbnail_image',
+            field=models.ImageField(upload_to=exchange.core.models.get_thumb_image_path),
+        ),
+    ]
