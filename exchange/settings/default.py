0237011109da60bbc98ed92fe2abb89f7971272f
William Nordmann <William.Nordmann@gmail.com>
2019-03-14 16:58:54 -0500

    Map of the Work spike

--- exchange/settings/default.py
+++ exchange/settings/default.py
@@ -225,7 +225,8 @@ else:
 
 if GEONODE_CLIENT_ENABLED:
     INSTALLED_APPS = ('geonode-client',) + INSTALLED_APPS
-    LAYER_PREVIEW_LIBRARY = 'react'
+    LAYER_PREVIEW_LIBRARY = 'MOW'
+    # LAYER_PREVIEW_LIBRARY = 'react'
 
 # authorized exempt urls
 ADDITIONAL_AUTH_EXEMPT_URLS = os.getenv(
