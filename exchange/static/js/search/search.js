7cdbaa4d481777b0e518c6a22f48b4653f960f44
William Nordmann <William.Nordmann@gmail.com>
2019-04-15 16:24:20 -0500

    Styling cleanup

--- exchange/static/js/search/search.js
+++ exchange/static/js/search/search.js
@@ -408,7 +408,7 @@
     * Spatial search
     */
     if ($('#preview_map').length > 0) {
-      var startingBounds = [-37.08984375,-33.046875,37.08984375,33.046875];
+      var startingBounds = [-45.87890625,-33.046875,45.87890625,33.046875];
       document.addEventListener("DOMContentLoaded", function (event) {
         MoW.ready(function() {
             var map = new MoW.Map({
